0dc6abb52d4ae2cfbfb50585ddb517d8b6c79f69
deverpham <deverpham@gmail.com>
2021-02-27 08:59:47 +0700

    rivision: completed 26/01

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Layout, Menu, Button } from "antd";
+import useLocalStorage from "react-use-localstorage";
+
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -9,46 +11,59 @@ import { useHistory } from "react-router-dom";
 const { Header, Sider, Content } = Layout;
 const Container = ({ children }) => {
   const [isCollapsed, setIsCollapsed] = React.useState(false);
+  const [isLogged, setIsLogged] = useLocalStorage("isLogged", "false");
   const history = useHistory();
   const toggle = () => {
     setIsCollapsed(!isCollapsed);
   };
-  return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={isCollapsed}>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />} active={true}>
-            Products
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header
-          className="site-layout-background header"
-          style={{ padding: 0 }}
-        >
-          {React.createElement(
-            isCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: toggle,
-            }
-          )}
-          <Button onClick={() => history.push("/login")}>Log out</Button>
-        </Header>
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          {children}
-        </Content>
+  const logout = () => {
+    setIsLogged("false");
+    history.push("/login");
+  };
+  useEffect(() => {
+    if (isLogged === "false") {
+      history.push("/login");
+    }
+  }, [isLogged]);
+  if (isLogged === "true") {
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={isCollapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />} active={true}>
+              Products
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+            className="site-layout-background header"
+            style={{ padding: 0 }}
+          >
+            {React.createElement(
+              isCollapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: toggle,
+              }
+            )}
+            <Button onClick={logout}>Logout</Button>
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {children}
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
-  );
+    );
+  }
+  return <></>;
 };
 export default Container;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Button } from "antd";
 import { Typography } from "antd";
 import { login } from "../services/api";
 import { useHistory } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
 import { toast } from "react-toastify";
 const Login = () => {
   const router = useHistory();
+  const [isLogged, setIsLogged] = useLocalStorage("isLogged", "false");
   const onFinish = (values) => {
     login(values).then(({ data: { isSuccess } }) => {
+      setIsLogged(true);
       if (isSuccess) {
         toast.success("Login successfully");
         router.push("/products");
@@ -16,47 +19,60 @@ const Login = () => {
       }
     });
   };
-  return (
-    <div className="login">
-      <Typography.Title className="login_title">Welcome!</Typography.Title>
-      <Typography.Text
-        style={{
-          color: "#9FA5AF",
-        }}
-      >
-        Sign in to your account
-      </Typography.Text>
-      <div class="login_form">
-        <Form
-          name="basic"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
+  useState(() => {
+    if (isLogged === "true") {
+      router.push("/products");
+    }
+  }, [isLogged]);
+  if (isLogged === "false") {
+    return (
+      <div className="login">
+        <Typography.Title className="login_title">Welcome!</Typography.Title>
+        <Typography.Text
+          style={{
+            color: "#9FA5AF",
+          }}
         >
-          <Form.Item
-            label="username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
+          Sign in to your account
+        </Typography.Text>
+        <div class="login_form">
+          <Form
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              label="username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            label="password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item
+              label="password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit">
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default Login;
